d4e0dd1ebadd0f5b8b7e567b52ffa484478bf5cb
YoonJuHwan <48649145+juhwandori@users.noreply.github.com>
2019-09-13 22:59:48 +0900

    장비 착용, 해제 / 상태이상 추상클래스 / Item 인터페이스 추가

---
+++ Rakshir/Assets/Scripts/PlayerEquipment.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEquipment : MonoBehaviour
+{
+    public enum EquipmentType
+    {
+        Weapon,
+        Armor,
+        Shoes,
+        Helmet
+    }
+
+    [SerializeField]
+    private List<IItem> listEquipment = new List<IItem>(4);
+
+    private void Awake()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            listEquipment.Add(null);
+        }
+    }
+
+    public void SetEquipment(IItem item, EquipmentType type)
+    {
+        if (listEquipment[(int)type] != null)
+            PopEquipment((int)type);
+
+        listEquipment[(int)type] = item;
+    }
+
+    private void PopEquipment(int index)
+    {
+        // Drop item (listEquipment[index])
+    }
+}
