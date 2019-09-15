22392302488d092f5a4f4525218907b3e6fd3017
YoonJuHwan <48649145+juhwandori@users.noreply.github.com>
2019-09-15 20:07:56 +0900

    Add LivingThing / PlayerStat

---
+++ Rakshir/Assets/Scripts/LivingThing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LivingThing : MonoBehaviour
+{
+    public float maxHp;
+
+    [SerializeField]
+    private float currentHp;
+
+    private void Awake()
+    {
+        currentHp = maxHp;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHp -= damage;
+
+        if (currentHp <= 0)
+            Dead();
+    }
+
+    protected abstract void Dead();
+
+    private void UpdateHealthVar()
+    {
+        // 체력바 업데이트
+    }
+
+    private void UpdateMaxHp(float updateValue)
+    {
+        maxHp = updateValue;
+        UpdateHealthVar();
+    }
+}
---
+++ Rakshir/Assets/Scripts/PlayerStat.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class PlayerStat : MonoBehaviour
+{
+    [ShowNativeProperty]
+    public int strength
+    {
+        set
+        {
+            m_strength = value;
+            hp += hpPerStr * value;
+            // Player.UpdateMaxHp(hp);
+            atk += atkPerStr * value;
+        }
+
+        get
+        {
+            return m_strength;
+        }
+    }
+    private int m_strength;
+
+    [ShowNativeProperty]
+    public int wisdom
+    {
+        set
+        {
+            m_wisdom = value;
+            cooldownReduce += cooldownReducePerWis * value;
+            skillAtk += skillAtkPerWis * value;
+        }
+
+        get
+        {
+            return m_wisdom;
+        }
+    }
+    private int m_wisdom;
+
+    [ShowNativeProperty]
+    public int dexterity
+    {
+        set
+        {
+            m_dexterity = value;
+            runSpeed += runSpeedPerDex * value;
+            atkSpeed += atkSpeedPerDex * value;
+        }
+
+        get
+        {
+            return m_dexterity;
+        }
+    }
+    private int m_dexterity;
+
+    [BoxGroup("Per Strength")]
+    [SerializeField]
+    private float hpPerStr;
+
+    [BoxGroup("Per Strength")]
+    [SerializeField]
+    private float atkPerStr;
+
+    [BoxGroup("Per Wisdom")]
+    [SerializeField]
+    private float cooldownReducePerWis;
+
+    [BoxGroup("Per Wisdom")]
+    [SerializeField]
+    private float skillAtkPerWis;
+
+    [BoxGroup("Per Dexterity")]
+    [SerializeField]
+    private float runSpeedPerDex;
+
+    [BoxGroup("Per Dexterity")]
+    [SerializeField]
+    private float atkSpeedPerDex;
+
+    [Space(10)]
+
+    [Header("Current Stat")]
+    public float hp;
+    public float atk;
+    public float cooldownReduce;
+    public float maxCooldownReduce;
+    public float skillAtk;
+    public float runSpeed;
+    public float atkSpeed;
+
+
+    private void Awake()
+    {
+        // hp = Player.maxHp;
+    }
+}
