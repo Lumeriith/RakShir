d291227309442bc1db4290024d5eb9485ec78554
Eunseop Shim <lumeriith@gmail.com>
2019-09-25 16:13:47 +0900

    Core Status Effect Implementation

---
+++ RakShir/Assets/CoreStatusEffect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum CoreStatusEffectType
+{
+    // Neutral
+    Stasis,
+
+    // Beneficial
+    Invulnerable, Untargetable, Protected, Speed, Haste,
+
+    // Harmful
+    Stun, Airborne, Sleep,
+
+    Polymorph,
+
+    Charm, Fear,
+
+    Root, Slow,
+
+    Silence, Blind
+}
+public class CoreStatusEffect
+{
+    public int uid;
+    public LivingThing caster;
+    public CoreStatusEffectType type;
+    public float duration;
+    public object parameter;
+
+    CoreStatusEffect(LivingThing caster, CoreStatusEffectType type, float duration, object parameter = null)
+    {
+        this.caster = caster;
+        this.type = type;
+        this.duration = duration;
+        this.parameter = null;
+    }
+
+    public bool IsHarmful()
+    {
+        return type >= CoreStatusEffectType.Stun && type <= CoreStatusEffectType.Blind;
+    }
+
+    public bool IsBeneficial()
+    {
+        return type >= CoreStatusEffectType.Invulnerable && type <= CoreStatusEffectType.Haste;
+    }
+}
+
