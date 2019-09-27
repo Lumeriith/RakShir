4eb5558b4a9d25c7e13da61bc878f508ea1675fd
Eunseop Shim <lumeriith@gmail.com>
2019-09-28 00:28:26 +0900

    Implemented several abilities

--- RakShir/Assets/ai_Vault.cs
+++ RakShir/Assets/ai_Vault.cs
@@ -7,15 +7,14 @@ public class ai_Vault : AbilityInstance
     public float vaultDistance = 2f;
     protected override void OnCreate(CastInfo castInfo, object[] data)
     {
-        if (photonView.IsMine)
-        {
-            float vaultSpeed = castInfo.owner.stat.finalMovementSpeed * 3;
-            
-            castInfo.owner.DashThroughWithSpeed(castInfo.owner.transform.position + castInfo.directionVector * vaultDistance, vaultSpeed);
-            castInfo.owner.control.StartChanneling(vaultDistance / (vaultSpeed/100));
-            castInfo.owner.control.basicAttackAbilityTrigger.ResetCooldown();
-        }
-        
+
+        float vaultSpeed = castInfo.owner.stat.finalMovementSpeed * 3;
+
+        castInfo.owner.DashThroughWithSpeed(castInfo.owner.transform.position + castInfo.directionVector * vaultDistance, vaultSpeed);
+        castInfo.owner.control.StartChanneling(vaultDistance / (vaultSpeed / 100), movable: true);
+        castInfo.owner.control.basicAttackAbilityTrigger.ResetCooldown();
+
+
         DetachChildParticleSystemsAndAutoDelete();
         DestroySelf();
     }
