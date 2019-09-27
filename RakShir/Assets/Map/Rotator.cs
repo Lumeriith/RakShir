8b359ef746af299c31314844d6a4beb3dce0ed70
JuHwanYoon <48649145+juhwandori@users.noreply.github.com>
2019-09-28 02:24:22 +0900

    Create rooms (teamA -> level1, level2)

---
+++ RakShir/Assets/Map/Rotator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpeed = 30f;
+    public enum RotateDirection { X, Y, Z };
+    public RotateDirection rotateDirection;
+
+    private void Start()
+    {
+        StartCoroutine("Rotate");
+    }
+
+    private IEnumerator Rotate()
+    {
+        while (true)
+        {
+            switch (rotateDirection)
+            {
+                case RotateDirection.X:
+                    transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime);
+                    break;
+                case RotateDirection.Y:
+                    transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+                    break;
+                case RotateDirection.Z:
+                    transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+                    break;
+            }
+
+            yield return null;
+        }
+    }
+}
