97ae60515809030462f97538684e0d1e6502ca97
Eunseop Shim <lumeriith@gmail.com>
2019-10-15 23:39:29 +0900

    Marker added

---
+++ DewDrop/Assets/Marker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(PhotonView))]
+public class Marker : MonoBehaviour
+{
+    Camera main;
+
+    private void Awake()
+    {
+        main = Camera.main;
+    }
+
+
+    void Update()
+    {
+        transform.rotation = main.transform.rotation;
+    }
+
+
+    public static void CreateMarker(string name, Vector3 position)
+    {
+        PhotonNetwork.Instantiate(name, )
+    }
+
+}
