47c38f67a3cb393d3a18f1b831f302a7f48067ef
JuHwanYoon <48649145+juhwandori@users.noreply.github.com>
2019-10-19 21:20:58 +0900

    Modify some room

--- DewDrop/Assets/Map/Rotator.cs
+++ DewDrop/Assets/Map/Rotator.cs
@@ -2,37 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Juhwan
+namespace Map
 {
     public class Rotator : MonoBehaviour
     {
         public float rotateSpeed = 30f;
         public enum RotateDirection { X, Y, Z };
-        public RotateDirection rotateDirection;
+        public List<RotateDirection> rotateDirections = new List<RotateDirection>();
 
-        private void Start()
+        private void Update()
         {
-            StartCoroutine("Rotate");
-        }
-
-        private IEnumerator Rotate()
-        {
-            while (true)
+            for (int i = 0; i < rotateDirections.Count; i++)
             {
-                switch (rotateDirection)
+                switch (rotateDirections[i])
                 {
                     case RotateDirection.X:
-                        transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime);
+                        transform.Rotate(Vector3.right, rotateSpeed * Time.deltaTime, Space.Self);
                         break;
                     case RotateDirection.Y:
-                        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+                        transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime, Space.Self);
                         break;
                     case RotateDirection.Z:
-                        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+                        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime, Space.Self);
                         break;
                 }
-
-                yield return null;
             }
         }
     }
