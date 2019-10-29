3cd72167538725e47fc3e4fa049c201b078deb7a
Eunseop Shim <lumeriith@gmail.com>
2019-10-29 13:53:56 +0900

    ManaPotion added & Potions behaviour adjusted

---
+++ DewDrop/Assets/ai_cons_ManaHeal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ai_cons_ManaHeal : AbilityInstance
+{
+    protected override void OnCreate(CastInfo castInfo, object[] data)
+    {
+        if (!photonView.IsMine) return;
+
+        info.owner.DoManaHeal((float)data[0], info.owner, true);
+        StartCoroutine(CoroutineDestroy());
+    }
+
+    protected override void AliveUpdate()
+    {
+        transform.position = info.owner.transform.position + info.owner.GetCenterOffset();
+    }
+
+    IEnumerator CoroutineDestroy()
+    {
+        yield return new WaitForSeconds(3f);
+        DetachChildParticleSystemsAndAutoDelete();
+        DestroySelf();
+    }
+}
---
+++ DewDrop/Assets/cons_ManaPotion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cons_ManaPotion : Consumable
+{
+    public float manahealAmount = 100f;
+    public override bool OnUse(CastInfo info)
+    {
+        AbilityInstanceManager.CreateAbilityInstance("ai_cons_ManaHeal", info.owner.transform.position + info.owner.GetCenterOffset(), Quaternion.identity, info, new object[] { manahealAmount });
+        DestroySelf();
+        return true;
+    }
+
+    public override bool IsReady()
+    {
+        return owner.stat.currentMana < owner.stat.finalMaximumMana;
+    }
+}
