b34cbb202fcb738f7e0dc0a77bcff65b61375f09
Eunseop Shim <lumeriith@gmail.com>
2019-11-02 01:10:33 +0900

    Camera Zoom Rubberbanding implemented

--- DewDrop/Assets/Scripts/Plugins/PlayerViewCamera.cs
+++ DewDrop/Assets/Scripts/Plugins/PlayerViewCamera.cs
@@ -5,33 +5,42 @@ using Cinemachine;
 using NaughtyAttributes;
 public class PlayerViewCamera : MonoBehaviour
 {
-    CinemachineVirtualCamera cvc;
+
     // Start is called before the first frame update
-    public Vector2 possibleCameraDistance;
+    public Vector2 desiredCameraDistance;
+    public Vector2 deadzoneCameraDistance;
     public float cameraDistanceStepSize;
+    public float clampingSpeed;
+
+    private CinemachineFramingTransposer cft;
+    private CinemachineVirtualCamera cvc;
 
     private void Awake()
     {
         cvc = GetComponent<CinemachineVirtualCamera>();
+        cft = cvc.GetCinemachineComponent<CinemachineFramingTransposer>();
     }
 
     private void Update()
     {
-        if(GameManager.instance.localPlayer != null)
+        if (GameManager.instance.localPlayer != null)
         {
             cvc.Follow = GameManager.instance.localPlayer.transform;
         }
 
-        if(Input.mouseScrollDelta.y < 0)
+        if (Input.mouseScrollDelta.y < 0)
         {
-            CinemachineFramingTransposer cft = cvc.GetCinemachineComponent<CinemachineFramingTransposer>();
-            cft.m_CameraDistance = Mathf.MoveTowards(cft.m_CameraDistance, possibleCameraDistance.y, cameraDistanceStepSize);
-        } else if (Input.mouseScrollDelta.y > 0)
-        {
-            CinemachineFramingTransposer cft = cvc.GetCinemachineComponent<CinemachineFramingTransposer>();
-            cft.m_CameraDistance = Mathf.MoveTowards(cft.m_CameraDistance, possibleCameraDistance.x, cameraDistanceStepSize);
+            cft.m_CameraDistance = Mathf.MoveTowards(cft.m_CameraDistance, deadzoneCameraDistance.y, cameraDistanceStepSize);
         }
+        else if (Input.mouseScrollDelta.y > 0)
+        {
+            cft.m_CameraDistance = Mathf.MoveTowards(cft.m_CameraDistance, deadzoneCameraDistance.x, cameraDistanceStepSize);
+        }
+        if (cft.m_CameraDistance > desiredCameraDistance.y) cft.m_CameraDistance = Mathf.MoveTowards(cft.m_CameraDistance, desiredCameraDistance.y, clampingSpeed * Time.deltaTime);
+        if (cft.m_CameraDistance < desiredCameraDistance.x) cft.m_CameraDistance = Mathf.MoveTowards(cft.m_CameraDistance, desiredCameraDistance.x, clampingSpeed * Time.deltaTime);
+
     }
+
 
 
 
