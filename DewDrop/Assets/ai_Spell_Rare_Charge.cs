53431455fb08c74361d44ab850ae1750ca39bcf1
Eunseop Shim <lumeriith@gmail.com>
2019-11-03 00:02:46 +0900

    6 Boots added

---
+++ DewDrop/Assets/ai_Spell_Rare_Charge.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+public class ai_Spell_Rare_Charge : AbilityInstance
+{
+    public SelfValidator selfValidator;
+    private ParticleSystem hit;
+    private ParticleSystem charge;
+    private new Collider collider;
+    private Vector3 chargeDestination;
+
+    public TargetValidator targetValidator;
+
+    public float chargeDelay = .5f;
+
+    public float chargeSpeed = 10f;
+    public float airborneDistance = 1f;
+    public float airborneDuration = 0.5f;
+    public float damage = 50f;
+    public float slowAmount = 30f;
+    public float slowDuration = 2f;
+    public float radius = 2.5f;
+    public float shieldAmount = 30f;
+    public float shieldDuration = 5f;
+
+    private Vector3 forwardVector;
+    protected override void OnCreate(CastInfo castInfo, object[] data)
+    {
+        hit = transform.Find("Hit").GetComponent<ParticleSystem>();
+        charge = transform.Find("Charge").GetComponent<ParticleSystem>();
+        collider = GetComponent<Collider>();
+        collider.enabled = false;
+        chargeDestination = castInfo.target.transform.position;
+        transform.LookAt(chargeDestination);
+        forwardVector = transform.forward;
+        forwardVector.y = 0f;
+        forwardVector.Normalize();
+        if (photonView.IsMine)
+        {
+            info.owner.control.StartChanneling(new Channel(selfValidator, 0.5f, false, false, false, false, ChannelFinished, ChannelCanceled));
+        }
+    }
+
+    private void ChannelFinished()
+    {
+        float duration = Vector3.Distance(chargeDestination, info.owner.transform.position) / chargeSpeed;
+        photonView.RPC("RpcCharge", RpcTarget.All);
+        info.owner.DashThroughForDuration(chargeDestination, duration);
+        info.owner.control.StartChanneling(new Channel(selfValidator, duration, false, false, false, false, ChannelCanceled, ChannelCanceled));
+        collider.enabled = true;
+        info.owner.PlayCustomAnimation("Rare - Charge");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isAlive) return;
+        if (!photonView.IsMine) return;
+        LivingThing thing = other.GetComponent<LivingThing>();
+        if (thing == null || !targetValidator.Evaluate(info.owner, thing)) return;
+
+        List<LivingThing> targets = info.owner.GetAllTargetsInRange(other.transform.position, radius, targetValidator);
+        info.owner.CancelDash();
+        for (int i = 0;i < targets.Count; i++)
+        {
+            photonView.RPC("RpcHit", RpcTarget.All, targets[i].photonView.ViewID);
+            info.owner.DoMagicDamage(damage, targets[i]);
+            targets[i].AirborneForDuration(targets[i].transform.position + forwardVector * airborneDistance, airborneDuration);
+            targets[i].ApplyStatusEffect(StatusEffect.Slow(info.owner, slowDuration, slowAmount));
+        }
+
+        if (targets.Count != 0)
+        {
+            info.owner.ApplyStatusEffect(StatusEffect.Shield(info.owner, shieldDuration, shieldAmount * targets.Count));
+        }
+
+        photonView.RPC("RpcStopCharge", RpcTarget.All);
+
+        DetachChildParticleSystemsAndAutoDelete();
+        DestroySelf();
+    }
+
+    [PunRPC]
+    private void RpcStopCharge()
+    {
+        if (!isAlive) return;
+        charge.Stop();
+    }
+
+
+    [PunRPC]
+    private void RpcCharge()
+    {
+        if (!isAlive) return;
+        charge.Play();
+    }
+
+    [PunRPC]
+    private void RpcHit(int viewID)
+    {
+        if (!isAlive) return;
+        LivingThing target = PhotonNetwork.GetPhotonView(viewID).GetComponent<LivingThing>();
+        Instantiate(hit.gameObject, target.transform.position + target.GetCenterOffset(), Quaternion.identity, transform).GetComponent<ParticleSystem>().Play();
+    }
+
+    private void ChannelCanceled()
+    {
+        if (!isAlive) return;
+        info.owner.CancelDash();
+        DetachChildParticleSystemsAndAutoDelete(DetachBehaviour.StopEmitting);
+        DestroySelf();
+    }
+
+    protected override void AliveUpdate()
+    {
+        transform.position = info.owner.transform.position;
+
+    }
+}
---
+++ DewDrop/Assets/trg_Spell_Rare_Charge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trg_Spell_Rare_Charge : AbilityTrigger
+{
+
+
+    public override void OnCast(CastInfo info)
+    {
+        CreateAbilityInstance("ai_Spell_Rare_Charge", transform.position, Quaternion.identity);
+        StartCooldown();
+        SpendMana();
+    }
+}
