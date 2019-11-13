3eda3f4d98318ba760e71a76d699e160d7cc772c
Eunseop Shim <lumeriith@gmail.com>
2019-11-14 02:28:55 +0900

    SFXManager, SFXInstance added

---
+++ DewDrop/Assets/SFXInstance.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXInstance : MonoBehaviour
+{
+    public Vector2 pitchRandomMultiplier;
+
+
+
+    private void Start()
+    {
+        
+    }
+}
---
+++ DewDrop/Assets/SFXManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager instance
+    {
+        get
+        {
+            if (_instance == null) _instance = FindObjectOfType<SFXManager>();
+            return _instance;
+        }
+    }
+    private static SFXManager _instance;
+
+    private Camera main;
+
+    private void Start()
+    {
+        main = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (GameManager.instance.localPlayer != null) transform.position = GameManager.instance.localPlayer.transform.position;
+        transform.rotation = main.transform.rotation;
+    }
+
+}
