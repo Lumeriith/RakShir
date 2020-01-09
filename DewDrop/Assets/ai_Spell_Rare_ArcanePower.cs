79b65fc5044090a3b7e95dc89421cc6f13cef182
Eunseop Shim <lumeriith@gmail.com>
2020-01-09 10:00:22 +0900

    Archmage Hood reworked

---
+++ DewDrop/Assets/ai_Spell_Rare_ArcanePower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ai_Spell_Rare_ArcanePower : AbilityInstance
+{
+    public TargetValidator targetValidator;
+    public float radius = 4f;
+    public float damage = 80f;
+    public float manaHealPerHit = 20f;
+    protected override void OnCreate(CastInfo castInfo, object[] data)
+    {
+        if (!photonView.IsMine) return;
+        List<LivingThing> targets = info.owner.GetAllTargetsInRange(transform.position, radius, targetValidator);
+        for(int i = 0; i < targets.Count; i++)
+        {
+            info.owner.DoMagicDamage(damage, targets[i]);
+        }
+        info.owner.DoManaHeal(manaHealPerHit * targets.Count, info.owner);
+        DetachChildParticleSystemsAndAutoDelete();
+        DestroySelf();
+    }
+}
