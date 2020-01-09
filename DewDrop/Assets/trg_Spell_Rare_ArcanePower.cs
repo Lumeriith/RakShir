0d12a8f68b89c3cb4160f873468d254047eeca3b
Eunseop Shim <lumeriith@gmail.com>
2020-01-09 10:01:49 +0900

    Archmage Hood reworked

---
+++ DewDrop/Assets/trg_Spell_Rare_ArcanePower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trg_Spell_Rare_ArcanePower : AbilityTrigger
+{
+    public override void OnCast(CastInfo info)
+    {
+        
+    }
+
+    public override void OnEquip()
+    {
+        if (owner.photonView.IsMine) owner.OnDealMagicDamage += DealtMagicDamage;
+    }
+
+    public override void OnUnequip()
+    {
+        if (owner.photonView.IsMine) owner.OnDealMagicDamage -= DealtMagicDamage;
+    }
+
+    private void DealtMagicDamage(InfoMagicDamage info)
+    {
+        if (!isCooledDown) return;
+        StartCooldown();
+        CreateAbilityInstance("ai_Spell_Rare_ArcanePower", info.to.transform.position, Quaternion.identity);
+    }
+
+}
