13097a8ba5de04473ffe85a098ed46395157e009
Eunseop Shim <lumeriith@gmail.com>
2020-02-05 07:42:29 +0900

    Transform class extended for generic Find function for components

---
+++ DewDrop/Assets/Scripts/Plugins/TransformGenericFindExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TransformGenericFindExtension
+{
+    public static T Find<T>(this Transform t, string name)
+    {
+        return t.Find(name).GetComponent<T>();
+    }
+}
