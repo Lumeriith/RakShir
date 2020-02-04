0a6a062df73648f0ac443369e6a60637aa2aafeb
Eunseop Shim <lumeriith@gmail.com>
2020-02-05 07:44:04 +0900

    FlamingBoots reworked; Added new spell: Breakthrough

---
+++ DewDrop/Assets/Scripts/AbilityInstances/ai_Spell_Rare_Breakthrough.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ai_Spell_Rare_Breakthrough : AbilityInstance
+{
+    public float dashDuration = 0.5f;
+    public float dashDistance = 5.5f;
+    public float slowAmount = 35f;
+    public float damage = 100f;
+    public float duration = 4f;
+
+    public TargetValidator targetValidator;
+
+    private ParticleSystem main;
+    private GameObject smallFire;
+
+    protected override void OnCreate(CastInfo info, object[] data)
+    {
+        transform.parent = info.owner.transform;
+        transform.position = info.owner.transform.position;
+        transform.rotation = info.directionQuaternion;
+        main = transform.Find<ParticleSystem>("Main");
+        smallFire = transform.Find("SmallFire").gameObject;
+        main.Play();
+        if (!isMine) return;
+        info.owner.StartDisplacement(new Displacement(info.directionVector * dashDistance, dashDuration, true, true, EasingFunction.Ease.Linear, Finished, Finished));
+    }
+
+    protected override void AliveUpdate()
+    {
+        transform.position = info.owner.transform.position;
+        transform.rotation = info.owner.transform.rotation;
+    }
+
+    private void Finished()
+    {
+        DetachChildParticleSystemsAndAutoDelete(DetachBehaviour.StopEmitting);
+        DestroySelf();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isMine) return;
+        LivingThing thing = other.GetComponent<LivingThing>();
+        if (thing == null || !targetValidator.Evaluate(info.owner, thing)) return;
+        thing.ApplyStatusEffect(StatusEffect.Slow(info.owner, duration, slowAmount));
+        thing.ApplyStatusEffect(StatusEffect.DamageOverTime(info.owner, duration, damage));
+        photonView.RPC("RpcSmallFire", RpcTarget.All, thing.photonView.ViewID);
+    }
+
+    [PunRPC]
+    private void RpcSmallFire(int id)
+    {
+        LivingThing thing = PhotonNetwork.GetPhotonView(id).GetComponent<LivingThing>();
+        GameObject newFire = Instantiate(smallFire, thing.transform.position + thing.GetCenterOffset(), Quaternion.identity, thing.transform);
+        newFire.GetComponent<ParticleSystem>().Play();
+        newFire.AddComponent<ParticleSystemAutoDestroy>();
+    }
+
+}
---
+++ DewDrop/Assets/Scripts/AbilityTriggers/trg_Spell_Rare_Breakthrough.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trg_Spell_Rare_Breakthrough : AbilityTrigger
+{
+    public float channelDuration = 0.25f;
+
+    public override void OnCast(CastInfo info)
+    {
+        info.owner.control.StartChanneling(new Channel(selfValidator, channelDuration, false, false, false, false, ChannelFinished, ChannelCanceled));
+    }
+
+    private void ChannelFinished()
+    {
+        CreateAbilityInstance("ai_Spell_Rare_Breakthrough", info.owner.transform.position, info.owner.transform.rotation, info);
+        SpendMana();
+        StartCooldown();
+    }
+
+    private void ChannelCanceled()
+    {
+        
+    }
+}
