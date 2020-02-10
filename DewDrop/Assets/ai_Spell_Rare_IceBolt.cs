6274982bd0b02649bb3ebdd6f973f2ef52e10dd8
Eunseop Shim <lumeriith@gmail.com>
2020-02-10 23:02:15 +0900

    FreezingCloak's new spell IceBolt added

---
+++ DewDrop/Assets/ai_Spell_Rare_IceBolt.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ai_Spell_Rare_IceBolt : AbilityInstance
+{
+    public Vector3 offset;
+
+    public float speed = 8f;
+    public float distance = 8f;
+
+    public float damage = 60f;
+    public float slowAmount = 40f;
+    public float slowDuration = 3f;
+    public float slowedEnemiesBonusMultiplier = 1.5f;
+
+    public TargetValidator validator;
+
+    private ParticleSystem start;
+    private ParticleSystem hit;
+
+    private Vector3 startPosition;
+
+
+    protected override void OnCreate(CastInfo info, object[] data)
+    {
+        start = transform.Find<ParticleSystem>("Start");
+        hit = transform.Find<ParticleSystem>("Hit");
+        start.Play();
+        transform.position += transform.rotation * offset;
+        startPosition = transform.position;
+        if (isMine) SFXManager.CreateSFXInstance("si_Spell_Rare_IceBolt Spawn", transform.position);
+    }
+
+    protected override void AliveUpdate()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+        if(isMine && Vector3.Distance(transform.position, startPosition) > distance)
+        {
+            DetachChildParticleSystemsAndAutoDelete(DetachBehaviour.StopEmitting);
+            DestroySelf();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isMine) return;
+        LivingThing thing = other.GetComponent<LivingThing>();
+        if (thing == null || !validator.Evaluate(info.owner, thing)) return;
+        if (thing.IsAffectedBy(StatusEffectType.Slow))
+        {
+            info.owner.DoMagicDamage(damage * slowedEnemiesBonusMultiplier, thing);
+        }
+        else
+        {
+            info.owner.DoMagicDamage(damage, thing);
+        }
+        SFXManager.CreateSFXInstance("si_Spell_Rare_IceBolt Hit", transform.position);
+        thing.ApplyStatusEffect(StatusEffect.Slow(info.owner, slowDuration, slowAmount));
+        photonView.RPC("RpcHit", RpcTarget.All);
+        DetachChildParticleSystemsAndAutoDelete(DetachBehaviour.DontStop);
+        DestroySelf();
+    }
+
+    [PunRPC]
+    private void RpcHit()
+    {
+        start.Stop();
+        hit.Play();
+    }
+
+}
---
+++ DewDrop/Assets/trg_Spell_Rare_IceBolt.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trg_Spell_Rare_IceBolt : AbilityTrigger
+{
+    public Vector3 offset;
+    public int iceboltCount = 3;
+    public float preDelay = 0.1f;
+    public float delayBetweenBolts = 0.25f;
+
+    public override void OnCast(CastInfo info)
+    {
+        StartCoroutine("CoroutineIceBolts");
+        SpendMana();
+        StartCooldown();
+        info.owner.control.StartChanneling(new Channel(selfValidator, iceboltCount * delayBetweenBolts + preDelay, true, true, true, false, null, ChannelCanceled));
+    }
+
+    private void ChannelCanceled()
+    {
+        StopCoroutine("CoroutineIceBolts");
+    }
+
+    IEnumerator CoroutineIceBolts()
+    {
+        yield return new WaitForSeconds(preDelay);
+        for(int i = 0; i < iceboltCount; i++)
+        {
+            CreateAbilityInstance("ai_Spell_Rare_IceBolt", info.owner.transform.position + offset, info.directionQuaternion, info);
+            yield return new WaitForSeconds(delayBetweenBolts);
+        }
+    }
+    
+}
