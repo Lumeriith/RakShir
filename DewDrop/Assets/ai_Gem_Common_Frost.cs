852745c4a2cfdb7bbfcf4ee12c1722cfe4911519
Eunseop Shim <lumeriith@gmail.com>
2020-02-20 09:03:58 +0900

    Added 3 new common gems

---
+++ DewDrop/Assets/ai_Gem_Common_Frost.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ai_Gem_Common_Frost : AbilityInstance
+{
+    protected override void OnCreate(CastInfo info, object[] data)
+    {
+        transform.position = info.target.transform.position + info.target.GetCenterOffset();
+        transform.parent = info.target.transform;
+        if (!isMine) return;
+        gem_Common_Frost frost = (gem_Common_Frost)source.gem;
+        info.target.ApplyStatusEffect(StatusEffect.Slow(source, frost.slowDuration[frost.level], frost.slowAmount[frost.level]));
+        DetachChildParticleSystemsAndAutoDelete();
+        DestroySelf();
+    }
+}
---
+++ DewDrop/Assets/gem_Common_Frost.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gem_Common_Frost : Gem
+{
+    public float[] slowDuration = { 2.0f, 2.5f, 3.0f, 3.5f, 4.0f };
+    public float[] slowAmount = { 20, 25, 30, 35, 40 };
+
+    public override void OnEquip(LivingThing owner, AbilityTrigger trigger)
+    {
+        if (owner.isMine) owner.OnDealMagicDamage += DealtMagicDamage;
+    }
+
+    public override void OnUnequip(LivingThing owner, AbilityTrigger trigger)
+    {
+        if (owner.isMine) owner.OnDealMagicDamage -= DealtMagicDamage;
+    }
+
+    private void DealtMagicDamage(InfoMagicDamage info)
+    {
+        if (info.source.trigger != trigger) return;
+        CreateAbilityInstance("ai_Gem_Common_Frost", info.to.transform.position + info.to.GetCenterOffset(), Quaternion.identity, CastInfo.OwnerAndTarget(owner, info.to));
+    }
+}
