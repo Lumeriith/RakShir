81c12f6d90265114feb89d580fc26063cef39427
Eunseop Shim <lumeriith@gmail.com>
2020-02-28 21:47:59 +0900

    Filesystem cleanly arranged

---
+++ DewDrop/Assets/Scripts/AbilityTriggers/trg_Spell_Rare_ShieldBash.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trg_Spell_Rare_ShieldBash : AbilityTrigger
+{
+    public float channelDuration = 0.1f;
+    public override void OnCast(CastInfo info)
+    {
+        owner.control.StartChanneling(new Channel(selfValidator, channelDuration, false, false, false, false, ChannelFinished, null));
+    }
+
+    private void ChannelFinished()
+    {
+        SpendMana();
+        StartCooldown();
+        CreateAbilityInstance("ai_Spell_Rare_ShieldBash", owner.transform.position, info.directionQuaternion, info);
+    }
+}
