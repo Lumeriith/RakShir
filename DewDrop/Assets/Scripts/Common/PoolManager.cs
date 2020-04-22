f72773a451987dc95a4405080961c7935d7ee9bb
Eunseop Shim <lumeriith@gmail.com>
2020-04-22 20:49:43 +0900

    PoolManager added

---
+++ DewDrop/Assets/Scripts/Common/PoolManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PoolManager : MonoBehaviour, IPunPrefabPool
+{
+    public void Destroy(GameObject gameObject)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public new GameObject Instantiate(string prefabId, Vector3 position, Quaternion rotation)
+    {
+        
+        throw new System.NotImplementedException();
+    }
+}
