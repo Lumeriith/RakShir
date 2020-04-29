31b9ef1964c88e267361d7bf463c1d44a0639e29
Eunseop Shim <lumeriith@gmail.com>
2020-04-30 00:30:30 +0900

    Replaced super-laggy Pixel Perfect with in-code screen point quantitization

---
+++ DewDrop/Assets/Scripts/Common/Vector3QuantitizeExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Vector3QuantitizeExtension
+{
+    /// <summary>
+    /// Returns a version of this vector rounded the coordinates to integer values.
+    /// </summary>
+    /// <param name="v"></param>
+    /// <returns></returns>
+    public static Vector3 Quantitized(this Vector3 v)
+    {
+        return new Vector3(Mathf.Round(v.x), Mathf.Round(v.y), Mathf.Round(v.z));
+    }
+
+    /// <summary>
+    /// Round this vector's coordinates to integer values.
+    /// </summary>
+    /// <param name="v"></param>
+    public static void Quantitize(this Vector3 v)
+    {
+        v.x = Mathf.Round(v.x);
+        v.y = Mathf.Round(v.y);
+        v.z = Mathf.Round(v.z);
+    }
+}
