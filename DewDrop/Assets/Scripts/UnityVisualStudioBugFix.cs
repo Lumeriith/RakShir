649adc79de37ddd58a1bb33750cff3b8a9c82d30
Eunseop Shim <lumeriith@gmail.com>
2020-05-07 03:35:22 +0900

    Unity QA is nonexistent

---
+++ DewDrop/Assets/Scripts/UnityVisualStudioBugFix.cs
@@ -0,0 +1,8 @@
+﻿using UnityEditor;
+public class FIX : AssetPostprocessor
+{
+    private static string OnGeneratedCSProject(string path, string content)
+    {
+        return content.Replace("<ReferenceOutputAssembly>false</ReferenceOutputAssembly>", "<ReferenceOutputAssembly>true</ReferenceOutputAssembly>");
+    }
+}
