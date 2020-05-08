7314f21b3858c881e99fb9266238d2bc979f421d
Eunseop Shim <lumeriith@gmail.com>
2020-05-09 02:37:53 +0900

    Upgraded to a newer version of Unity

--- DewDrop/Assets/Scripts/UnityVisualStudioBugFix.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEditor;
-public class FIX : AssetPostprocessor
-{
-    private static string OnGeneratedCSProject(string path, string content)
-    {
-        return content.Replace("<ReferenceOutputAssembly>false</ReferenceOutputAssembly>", "<ReferenceOutputAssembly>true</ReferenceOutputAssembly>");
-    }
-}
