d6e36341da1331ba545265d995c3fa523c0a1c58
Eunseop Shim <lumeriith@gmail.com>
2020-05-24 04:41:18 +0900

    Added convenient extensions

---
+++ DewDrop/Assets/Scripts/Common/QuaternionFlattenExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class QuaternionFlattenExtension
+{
+    /// <summary>
+    /// Flattens this quaternion, leaving only its y-axis rotation.
+    /// </summary>
+    /// <param name="q"></param>
+    public static void Flatten(this ref Quaternion q)
+    {
+        q = Quaternion.Euler(0f, q.eulerAngles.y, 0f);
+    }
+
+    /// <summary>
+    /// Returns a flattened version of this quaternion, leaving only its y-axis rotation.
+    /// </summary>
+    /// <param name="q"></param>
+    /// <returns></returns>
+    public static Quaternion Flattened(this Quaternion q)
+    {
+        return Quaternion.Euler(0f, q.eulerAngles.y, 0f);
+    }
+}
---
+++ DewDrop/Assets/Scripts/Common/RandomExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RandomExtension
+{
+    /// <summary>
+    /// Returns a rotation that deviates with a random angle using provided parameters.
+    /// </summary>
+    /// <param name="minAngle"></param>
+    /// <param name="maxAngle"></param>
+    /// <returns></returns>
+    public static Quaternion Spread(float minAngle, float maxAngle)
+    {
+        return Quaternion.AngleAxis(Random.Range(0f, 360f), Vector3.forward) * Quaternion.AngleAxis(Random.Range(minAngle, maxAngle), Vector3.up);
+    }
+}
